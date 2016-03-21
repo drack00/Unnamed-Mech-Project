a334e697bb1f1a1fd37b4675d832c508154947ad
drack00 <uncooldan@live.ca>
2016-03-20 21:12:22 -0300

    Mouse look, basic layout, and lighting.

---
+++ Assets/Scripts/LightCuller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCuller : MonoBehaviour 
+{
+	new Camera camera
+	{
+		get
+		{
+			return GetComponent<Camera> ();
+		}
+	}
+
+	void OnPreCull ()
+	{
+		foreach(Light light in FindObjectsOfType<Light> ())
+		{
+			if((camera.cullingMask & light.cullingMask) == 0)light.enabled = false;
+		}
+	}
+	void OnPostRender ()
+	{
+		foreach(Light light in FindObjectsOfType<Light> ())
+		{
+			light.enabled = true;
+		}
+	}
+}
