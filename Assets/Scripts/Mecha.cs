0fe2a6fd3b224787ab3ddf27f011faf1068d9bd9
drack00 <uncooldan@live.ca>
2016-03-29 20:51:36 -0300

    Negative movement inputs are now working as intended.

--- Assets/Scripts/Mecha.cs
+++ Assets/Scripts/Mecha.cs
@@ -15,6 +15,7 @@ public class Mecha : MonoBehaviour
 	{
 		get
 		{
+			if(Mathf.Approximately(leftBiasInput.floatValue, 0f))return InputBiasSign.Zero;
 			if(leftBiasInput.floatValue > 0.0f)return InputBiasSign.Positive;
 			if(leftBiasInput.floatValue < 0.0f)return InputBiasSign.Negative;
 			return InputBiasSign.Zero;
@@ -24,6 +25,7 @@ public class Mecha : MonoBehaviour
 	{
 		get
 		{
+			if(Mathf.Approximately(rightBiasInput.floatValue, 0))return InputBiasSign.Zero;
 			if(rightBiasInput.floatValue > 0.0f)return InputBiasSign.Positive;
 			if(rightBiasInput.floatValue < 0.0f)return InputBiasSign.Negative;
 			return InputBiasSign.Zero;
@@ -34,20 +36,25 @@ public class Mecha : MonoBehaviour
 		get
 		{
 			float xBias = 0.0f;
+			xBias += leftBiasSign != InputBiasSign.Negative ? 
+				-1.0f * Mathf.Abs(leftBiasInput.floatValue) : 
+				-1.0f * Mathf.Abs(leftBiasInput.floatValue) / 2.0f;
+			xBias += rightBiasSign != InputBiasSign.Negative ? 
+				Mathf.Abs(rightBiasInput.floatValue) : 
+				Mathf.Abs(rightBiasInput.floatValue) / 2.0f;
+			
 			float yBias = 0.0f;
-			if(leftBiasSign != InputBiasSign.Negative)xBias += -1.0f * leftBiasInput.floatValue;
-			if(rightBiasSign != InputBiasSign.Negative)xBias += rightBiasInput.floatValue;
 			if(leftBiasSign != InputBiasSign.Negative && rightBiasSign != InputBiasSign.Negative)yBias = ((2.0f * leftBiasInput.floatValue) * (2.0f * rightBiasInput.floatValue)) / 2.0f;
-			else yBias = -1 * (Mathf.Abs(2.0f * leftBiasInput.floatValue) * Mathf.Abs(2.0f * rightBiasInput.floatValue)) / 2.0f;
+			else yBias = -1 * (Mathf.Abs (leftBiasInput.floatValue) + Mathf.Abs (rightBiasInput.floatValue)) / 2.0f;
 
-			Vector2 _inputDir = new Vector2 (xBias, yBias);
-
-			return _inputDir;
+			return new Vector2 (xBias, yBias);
 		}
 	}
 
+	public float moveSpeed;
+
 	void Update ()
-	{
-		Debug.Log ("final " + inputDir);
+	{Debug.Log (inputDir);
+		//transform.position += new Vector3(inputDir.x, 0.0f, inputDir.y) * moveSpeed;
 	}
 }
