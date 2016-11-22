344555b7401db896e84cc377c7dfde358b871ace
drack00 <uncooldan@live.ca>
2016-11-22 19:33:35 -0400

    Prep for remote.

--- Assets/Scripts/Mecha.cs
+++ Assets/Scripts/Mecha.cs
@@ -94,25 +94,43 @@ public class Mecha : MonoBehaviour
 			else target = targets [0];
 		}
 	}
-
-	public float reflectSpeed;
-	void OnCollisionEnter (Collision other)
+		
+	void OnControllerColliderHit (ControllerColliderHit hit)
 	{
-		if(other.gameObject.GetComponent<Mecha> () != null)
+		if(hit.gameObject.GetComponent<Enviroment> () != null)
 		{
-			//Mecha mecha = collision.gameObject.GetComponent<Mecha> ();
-			Vector3 reflect = Vector3.Reflect (transform.TransformDirection (Vector3.forward), other.contacts [0].normal);
-			transform.position += reflect * reflectSpeed;
-		}
-		if(other.gameObject.GetComponent<Enviroment> () != null)
-		{
-			//Enviroment enviroment = collision.gameObject.GetComponent<Enviroment> ();
-			Vector3 reflect = Vector3.Reflect (transform.TransformDirection (Vector3.forward), other.contacts [0].normal);
-			transform.position += reflect * reflectSpeed;
+			Enviroment enviroment = hit.gameObject.GetComponent<Enviroment> ();
+			if(enviroment.type == Enviroment.Type.Static)Bounce (hit);
 		}
 	}
 		
-	void FixedUpdate ()
+	private void Bounce (ControllerColliderHit hit)
+	{
+		status |= Status.Prone;
+		force += Vector3.Reflect (hit.moveDirection, hit.normal) * hit.moveLength * forceFalloff;
+	}
+
+	public float recoveryTime;
+	[System.Serializable][System.Flags]public enum Status
+	{
+		Prone = 0x0001,
+		Recovery = 0x0002
+	}
+	private Status status;
+	private IEnumerator Recovering ()
+	{
+		status |= Status.Recovery;
+		float _recoveryTime = recoveryTime;
+		while (_recoveryTime > 0.0f)
+		{
+			_recoveryTime -= Time.fixedDeltaTime;
+			yield return new WaitForFixedUpdate ();
+		}
+		status ^= Status.Prone;
+		status ^= Status.Recovery;
+	}
+		
+	private void Control (out Vector3 position, out Quaternion rotation)
 	{
 		Transform _transform = new GameObject ().transform;
 		_transform.position = transform.position;
@@ -126,11 +144,34 @@ public class Mecha : MonoBehaviour
 		}
 		else 
 		{
-			_transform.Rotate (transform.TransformDirection (Vector3.up), inputDir.x * rotSpeed * Time.fixedDeltaTime);
+			_transform.Rotate (_transform.TransformDirection (Vector3.up), inputDir.x * rotSpeed * Time.fixedDeltaTime);
 			_transform.position += _transform.TransformDirection (Vector3.forward) * inputDir.y * moveSpeed * Time.fixedDeltaTime;
 		}
-		GetComponent<Rigidbody> ().MovePosition (_transform.position);
-		GetComponent<Rigidbody> ().MoveRotation (_transform.rotation);
+		position = _transform.position;
+		rotation = _transform.rotation;
 		Destroy (_transform.gameObject);
+	}
+
+	public float gravity;
+	private Vector3 force;
+	public float forceFalloff;
+	void FixedUpdate ()
+	{
+		Vector3 position = transform.position;
+		Quaternion rotation = transform.rotation;
+
+		if((status & Status.Prone) == 0) 
+		{
+			Control (out position, out rotation);
+		}
+
+		if(!GetComponent<CharacterController> ().isGrounded)position += Vector3.down * gravity * Time.fixedDeltaTime;
+		position += force;
+		force *= forceFalloff;
+		if(force.magnitude < 1.0f)force = Vector3.zero;
+		if(Mathf.Approximately(force.magnitude, Mathf.Epsilon) && (status & Status.Prone) != 0 && (status & Status.Recovery) == 0)StartCoroutine (Recovering ());
+
+		GetComponent<CharacterController> ().Move (position - transform.position);
+		transform.rotation = rotation;
 	}
 }
